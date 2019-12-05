4fd20c99209f4bc1d73f3f18a0ef0dc89ddaf871
Sam Jespersen <heyheysamj@gmail.com>
2019-12-05 15:16:34 -0800

    sockets are working

---
+++ src/socket.js
@@ -0,0 +1,43 @@
+//RYANS KILLER SOCKET CODE COPYRIGHT RYAN 5EVER
+
+import React, { createContext, useContext, useReducer, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('ws://localhost:7890');
+const SocketContext = createContext(socket);
+
+export const SocketProvider = ({ children }) => {
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
+
+export const useEmitEvent = eventName => {
+  const socket = useSocket();
+  return data => {
+    socket.emit(eventName, data);
+  };
+};
+
+export const useOnEvent = (reducer, eventNames) => {
+  const socket = useSocket();
+  const [state, dispatch] = useReducer(reducer, {});
+
+  useEffect(() => {
+    eventNames.forEach(eventName => {
+      socket.on(eventName, payload => dispatch({
+        type: eventName,
+        payload
+      }));
+    });
+  });
+
+  return state;
+};
