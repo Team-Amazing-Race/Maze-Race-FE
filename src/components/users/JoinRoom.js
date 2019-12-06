2512bec400964f4541f65f95a157699aa3883121
Sam Jespersen <heyheysamj@gmail.com>
2019-12-06 14:01:15 -0800

    rooms working!

---
+++ src/components/users/JoinRoom.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const JoinRoomForm = ({ handleJoin }) => {
+
+  const [name, setName] = useState('');
+
+  return (
+    <form onSubmit={(event) => handleJoin(event, name)}>
+      <input type="text" value={name} onChange={({ target }) => setName(target.value)} />
+      <button>Start</button>
+    </form>
+  );
+
+};
+
+JoinRoomForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired
+};
+
+export default JoinRoomForm;
