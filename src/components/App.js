5739f9ba5cfad91484a19ef3c8612a282d4e799c
Kamran Ali <kgali21@gmail.com>
2019-12-06 17:43:56 -0800

    cheacking to see if the lobbies work with everything

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,38 @@ import Form from './users/Form';
 import { reducer } from '../reducers/reducer';
 import JoinRoomForm from './users/JoinRoom';
 import LobbyList from './users/LobbyList';
+import EnterName from './users/EnterName';
 
 
 
 export default function App() {
 
   //Listen for events from the backend that will hit the reducer
-  const eventState = useOnEvent(reducer, ['ROOM_JOIN_DONE']);
+  const eventState = useOnEvent(reducer, ['ROOM_JOIN_DONE', 'ENTER_NAME_DONE']);
 
   //Actions to send to the backend
-  const joinRoom = useEmitEvent('ROOM_JOIN', 'ROOM_DISCONNECT');
+  const joinRoom = useEmitEvent('ROOM_JOIN');
+  const enterName = useEmitEvent('ENTER_NAME');
 
   const handleSubmit = (event, data) => {
     event.preventDefault();
-    joinRoom({ room: data });
+    joinRoom({ room: data, name: eventState.name });
     console.log(eventState.rooms);
+  };
+
+  const handleName = (event, data) => {
+    event.preventDefault();
+    enterName({ name: data });
+    console.log(eventState);
   };
 
 
   return (
     <>
+      <EnterName handleName={handleName} />
+      <span>Your name is {eventState.name}</span>
       <p>make a room ya dingus</p>
       <Form handleSubmit={handleSubmit} />
-
       <LobbyList rooms={eventState.rooms} />
 
       <br />
--- src/components/users/Lobby.js
+++ src/components/users/Lobby.js
@@ -2,10 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Lobby = ({ name, players }) => {
+  console.log(players);
+  const playerString = players.reduce((acc, val) => {
+    acc += val.name + ', ';
+    return acc;
+  }, '');
   return (
     <>
       <p>Name: {name}</p>
-      <p>Players: {players}</p>
+      <p>Players: {playerString}</p>
     </>
   );
 
