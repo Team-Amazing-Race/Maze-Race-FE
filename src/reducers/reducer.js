1f7a4e8abf302630adaa24eeb66b682c1b68de5e
Kamran Ali <kgali21@gmail.com>
2019-12-06 17:43:56 -0800

    cheacking to see if the lobbies work with everything

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,26 +1,47 @@
 export const reducer = (state, { type, payload }) => {
   switch(type) {
     case 'ROOM_JOIN_DONE': {
-      if(state.rooms.some(room => {
-        return room.name === payload;
+      const { room, name } = payload;
+      const inRoom = () => {
+        const foundRoom = state.rooms.find(openRoom => {
+          return openRoom.name === room;
+        });
+        return foundRoom.players.some(player => {
+          return player.name === name;
+        });
+      };
+      if(state.rooms.some(currentRoom => {
+        return currentRoom.name === room && !inRoom();
       })) {
-        const rooms = state.rooms.map(room => {
-          if(room.name === payload) {
-            return { ...room, players: room.players + 1 };
+        const rooms = state.rooms.map(currentRoom => {
+          if(currentRoom.name === room) {
+            return { ...currentRoom, players: [...state.currentRoom.players, { name: name }] };
           } else {
-            return room;
+            return currentRoom;
           }
         });
         return { ...state, rooms };
       }
 
-      return { ...state, rooms: [...state.rooms, { name: payload, players: 1 }] };
+      return {
+        ...state,
+        rooms: [...state.rooms,
+          {
+            name: room,
+            players: [
+              { name: name }
+            ]
+          }
+        ]
+      };
     }
     case 'ROOM_DISCONNECT': {
       console.log('ROOM DISCONNECT');
       return state;
     }
-
+    case 'ENTER_NAME_DONE':
+      console.log('here we are');
+      return { ...state, name: payload };
     default:
       return state;
   }
