d5fc105f276c91d365baf9519d0af3118f6d9377
Kamran Ali <kgali21@gmail.com>
2019-12-10 15:41:34 -0800

    got radio button styling to work

--- src/components/users/PlayerSelection.js
+++ src/components/users/PlayerSelection.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import styles from '../styles/PlayerSelection.css'
+import styles from '../styles/PlayerSelection.css';
 
 const PlayerSelection = ({ handleSubmit, colors, symbols }) => {
 
   const [name, setName] = useState('');
 
   const selectionElements = colors.map((color, i) => (
-    <label key={i} style={{ backgroundColor: color }} className={styles.Label}> 
-      {symbols[i]}
-      <input type="radio" id="selection"  value={color + symbols[i]} className={styles.Radio}/>
-    </label>
+    <>
+      <input type="radio" id={color} value={color + symbols[i]} className={styles.Radio} name="playerSelections" />
+      <label key={i} style={{ backgroundColor: color }} className={styles.Label} htmlFor={color}>
+        {symbols[i]}
+      </label>
+    </>
   ));
 
   return (
     <form onSubmit={(event) => handleSubmit(event, name)}>
       <input className={styles.PlayerName} type="text" value={name} onChange={({ target }) => setName(target.value)} />
-      <div>
+      <div className={styles.ButtonContainer}>
         {selectionElements}
       </div>
       <button>Start</button>
@@ -28,7 +30,7 @@ const PlayerSelection = ({ handleSubmit, colors, symbols }) => {
 PlayerSelection.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   colors: PropTypes.arrayOf(PropTypes.string.isRequired),
-  symbols: PropTypes.arrayOf(PropTypes.string.isRequired) 
+  symbols: PropTypes.arrayOf(PropTypes.string.isRequired)
 };
 
 export default PlayerSelection;
