7037dac99dbc0af73dc4cfb57ab25088ae633bf4
Sam Jespersen <heyheysamj@gmail.com>
2019-12-09 17:58:13 -0800

    app logic into game

--- src/components/App.js
+++ src/components/App.js
@@ -1,94 +1,13 @@
 import React from 'react';
-import { useEmitEvent, useOnEvent } from '../socket';
-import Form from './users/Form';
-import { reducer } from '../reducers/reducer';
-import LobbyList from './users/LobbyList';
-import EnterName from './users/EnterName';
-import PlayerSelection from './users/PlayerSelection';
-import PlayersList from './users/PlayerList';
-import PlayersForm from './users/PlayersForm';
-import ResultMessage from './users/ResultMessage';
+import Game from '../container/Game';
 
 
 export default function App() {
 
-  //Listen for events from the backend that will hit the reducer
-  const eventState = useOnEvent(reducer,
-    ['ROOM_JOIN_DONE',
-      'ENTER_NAME_DONE',
-      'MOVE_PLAYER_DONE',
-      'ROOM_JOIN_PRIVATE']);
-
-  //Actions to send to the backend
-  const joinRoom = useEmitEvent('ROOM_JOIN');
-  const enterName = useEmitEvent('ENTER_NAME');
-  const movePlayer = useEmitEvent('MOVE_PLAYER');
-
-  const handleSubmit = (event, data) => {
-    event.preventDefault();
-    joinRoom({ room: data, name: eventState.name });
-  };
-
-  const handleName = (event, data) => {
-    event.preventDefault();
-    enterName({ name: data });
-
-  };
-
-  const props = { 
-    colors: ['black', 'red', 'blue', 'green'], 
-    symbols: ['Q', 'Z', 'W', 'P'],
-    players: [{
-      name: 'Jose',
-      color: 'red',
-      symbol: '!'
-    }, {
-      name: 'Dylan',
-      color: 'blue',
-      symbol: '!'
-    }],
-    winner: 'Dylan'
-  };
-
-  const keyDownListener = (event) => {
-    const keyName = event.key;
-    if(keyName === 'ArrowUp') {
-      movePlayer({ dir: 'up', name: eventState.name, room: eventState.inRoom });
-    }
-    if(keyName === 'ArrowDown') {
-      movePlayer({ dir: 'down', name: eventState.name, room: eventState.inRoom });
-    }
-    if(keyName === 'ArrowRight') {
-      movePlayer({ dir: 'right', name: eventState.name, room: eventState.inRoom });
-    }
-    if(keyName === 'ArrowLeft') {
-      movePlayer({ dir: 'left', name: eventState.name, room: eventState.inRoom });
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('keydown', keyDownListener);
-    return () => {
-      window.removeEventListener('keydown', keyDownListener);
-    };
-  });
-
   return (
-    <div>
-      <EnterName handleName={handleName} />
-      <p>make a room ya dingus</p>
-      <Form handleSubmit={handleSubmit} />
-      <LobbyList rooms={eventState.rooms} />
-      <br />
-
-      <p>join a room ya idjit</p>
-      <JoinRoomForm handleJoin={handleSubmit} />
-
-      <PlayersForm handleSubmit={handleSubmit} />
-      <PlayerSelection handleSubmit={handleSubmit} colors={props.colors} symbols={props.symbols} />
-      <PlayersList players={props.players} />
-      <ResultMessage handleSubmit={handleSubmit} name={props.players[0].name} winner={props.winner} />
-    </div>
+    <>
+      <Game />
+    </>
 
   );
 }
--- src/container/Game.js
+++ src/container/Game.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useEmitEvent, useOnEvent } from '../socket';
+import { reducer } from '../reducers/reducer';
+
 import PlayersForm from '../components/users/PlayersForm';
 import PlayerSelection from '../components/users/PlayerSelection';
 import PlayersList from '../components/users/PlayerList';
@@ -6,20 +9,44 @@ import ResultMessage from '../components/users/ResultMessage';
 import Modal from '../components/Modal';
 
 const Game = () => {
+  // State
   const [players, setPlayers] = useState(null);
   const [isOpen, setIsOpen] = useState(true);
   const [winner, setWinner] = useState(null);
 
-  let children;
-  const colors = ['black', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-  const symbols = ['Q', 'Z', 'W', 'P', 'K', 'M', 'B', 'S'];
+
+  //Listen for events from the backend that will hit the reducer
+  const eventState = useOnEvent(reducer,
+    ['ROOM_JOIN_DONE',
+      'ENTER_NAME_DONE',
+      'MOVE_PLAYER_DONE',
+      'ROOM_JOIN_PRIVATE']);
+
+  //Actions to send to the backend
+  const joinRoom = useEmitEvent('ROOM_JOIN');
+  const enterName = useEmitEvent('ENTER_NAME');
+  const movePlayer = useEmitEvent('MOVE_PLAYER');
+
+  //Handlers
+
+  const handleSubmit = (event, data) => {
+    event.preventDefault();
+    joinRoom({ room: data, name: eventState.name });
+  };
+
+  const handleName = (event, data) => {
+    event.preventDefault();
+    enterName({ name: data });
+
+  };
+
   const handleNewGame = (event, number) => {
     event.preventDefault();
     setPlayers(number);
   };
 
   const handleReset = () => {
-    
+
   };
 
   const handlePlayerSelect = (event, data) => {
@@ -27,23 +54,67 @@ const Game = () => {
     setPlayers(data);
   };
 
+
+  //variables
+  let children;
+  const colors = ['black', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  const symbols = ['Q', 'Z', 'W', 'P', 'K', 'M', 'B', 'S'];
+
+
+  //Modal display logic
+
   if(!players && isOpen) {
+    console.log('here!!');
     children = (
-      <h1>Logo!</h1>,
-      <PlayersForm handleSubmit={handleNewGame} />
+      <>
+        <h1>Logo!</h1>
+        <PlayersForm handleSubmit={handleNewGame} />
+      </>
     );
   }
 
   if(players && isOpen && !winner) {
     children = (
-      <PlayerSelection colors={colors} symbols={symbols} handleSubmit={handlePlayerSelect} />,
-      <PlayersList players={players} />
+      <>
+        <PlayerSelection colors={colors} symbols={symbols} handleSubmit={handlePlayerSelect} />
+        <PlayersList players={[{ name: 'poop', color: 'brown', symbol: '$' }]} />
+      </>
     );
   }
 
   if(winner && isOpen) {
     children = <ResultMessage winner={winner} handleSubmit={handleReset} />;
   }
+
+
+  //Keypress logic
+
+  const keyDownListener = (event) => {
+    const keyName = event.key;
+    if(keyName === 'ArrowUp') {
+      movePlayer({ dir: 'up', name: eventState.name, room: eventState.inRoom });
+    }
+    if(keyName === 'ArrowDown') {
+      movePlayer({ dir: 'down', name: eventState.name, room: eventState.inRoom });
+    }
+    if(keyName === 'ArrowRight') {
+      movePlayer({ dir: 'right', name: eventState.name, room: eventState.inRoom });
+    }
+    if(keyName === 'ArrowLeft') {
+      movePlayer({ dir: 'left', name: eventState.name, room: eventState.inRoom });
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', keyDownListener);
+    return () => {
+      window.removeEventListener('keydown', keyDownListener);
+    };
+  });
+
+  useEffect(() => {
+
+  });
 
   return (
     <Modal>
