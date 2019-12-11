83b461845994cef76d29f05a3a29f73d8de5c62e
Jose <j.a.ojeda@live.com>
2019-12-10 17:58:44 -0800

    started work on Lobby.js

--- src/container/Lobby.js
+++ src/container/Lobby.js
@@ -1,3 +1,31 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import PlayersForm from '../components/users/PlayersForm';
+import PlayersList from '../components/users/PlayersList';
+import { useGameState } from '../socket';
+import useGameEmitters from '../components/hooks/gameState';
+
+const Lobby = ({ match, history }) => {
+
+  const { setUserId, joinRoomPrivate, enterName, joinRoom } = useGameEmitters();
+  const eventState = useGameState();
+
+
+  return (
+    <>
+      <PlayersForm handleSubmit={handleName} type="text" />
+      <PlayersList players={getPlayers(eventState)} />
+    </>
+  );
+}
+
+Lobby.propTypes = {
+  match: PropTypes.obj,
+  history: PropTypes.object.isRequired
+};
+
+export default Lobby;
+
 // if (!eventState.userId) {
 //     const id = shortId.generate();
 //     setUserId(id);
