3e1b8b9b31087fda5c26b12420fd4f46ed2a5e8e
Kamran Ali <kgali21@gmail.com>
2019-12-11 12:50:32 -0800

    starting to style, got it to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Game from '../container/Game';
-import PlayerSelection from '../components/users/PlayerSelection';
+// import Game from '../container/Game';
+// import PlayerSelection from '../components/users/PlayerSelection';
 import About from './users/About';
 import aboutProps from '../components/users/AboutProps.json';
 
 
 export default function App() {
-  const color = ['red', 'green', 'blue', 'purple'];
-  const symbols = ['√', '◊', 'Ó', ''];
-  const handleSubmit = () => {
-    console.log('a little sumpin sumpin');
-  };
+  // const color = ['red', 'green', 'blue', 'purple'];
+  // const symbols = ['√', '◊', 'Ó', ''];
+  // const handleSubmit = () => {
+  //   console.log('a little sumpin sumpin');
+  // };
 
   return (
     <>
       {/* <PlayerSelection handleSubmit={handleSubmit} colors={color} symbols={symbols} /> */}
       {/* <Game /> */}
-      <About name={aboutProps.name} img={aboutProps.img} desc={aboutProps.desc} />
+      <About name={aboutProps.name} img={aboutProps.image} desc={aboutProps.desc} />
     </>
 
   );
