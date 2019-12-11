9245641c85679685e75f85b0dd0e9738a26ef557
Jose <j.a.ojeda@live.com>
2019-12-10 17:51:31 -0800

    wrote Home.js and refactored home code

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,20 +1,34 @@
-// const handleRoomCreate = (event, number) => {
-//     event.preventDefault();
-//     const roomId = shortId.generate();
+import React from 'react';
+import PropTypes from 'prop-types';
+import PlayersForm from '../components/users/PlayersForm';
+import { useGameState } from '../socket';
+import useGameEmitters from '../components/hooks/gameState';
 
-//     createRoom({ room: roomId, number: number, userId: eventState.userId });
-//     history.push(`/${roomId}`);
-// };
+const shortId = require('shortid');
 
+const Home = ({ history }) => {
 
+  const { createRoom } = useGameEmitters();
+  const eventState = useGameState();
+  
+  const handleRoomCreate = (event, number) => {
+    event.preventDefault();
+    const roomId = shortId.generate();
+  
+    createRoom({ room: roomId, number: number, userId: eventState.userId });
+    history.push(`/${roomId}`);
+  };
 
-// if (isOpen && !match.params.roomId) {
-//     children = (
-//         <>
-//             <h1>Logo!</h1>
-//             <PlayersForm handleSubmit={handleRoomCreate} type="number" />
-//         </>
-//     );
-// }
+  return (
+    <>
+      <h1>Logo!</h1>
+      <PlayersForm handleSubmit={handleRoomCreate} type="number" />
+    </>
+  );
+};
 
-// const shortId = require('shortid');
+Home.propTypes = {
+  history: PropTypes.object.isRequired
+};
+
+export default Home;
