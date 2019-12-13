a9e25869accaaa6f615a5b211ae53a1ac6e59339
Jose <j.a.ojeda@live.com>
2019-12-12 20:36:14 -0800

    added win condition and created Results.js

---
+++ src/components/users/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Home from '../../container/Home';
+
+const Results = ({ match, history }) => {
+
+};
+
+Results.propTypes = {
+  match: PropTypes.obj,
+  history: PropTypes.object.isRequired
+};
+
+export default Results;
