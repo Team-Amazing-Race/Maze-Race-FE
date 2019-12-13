5ce3d4b4d54e75c04bb5110a7f2ee72fa5964c80
Sam Jespersen <heyheysamj@gmail.com>
2019-12-12 18:19:56 -0800

    player movement almost

--- src/components/sketch/Player.js
+++ src/components/sketch/Player.js
@@ -1,5 +1,5 @@
 export default class Player {
-  constructor(p, xPos, yPos, x, y, w, h) {
+  constructor(p, xPos, yPos, x, y, w, h, id) {
     this.x = x;
     this.y = y;
     this.w = w;
@@ -7,46 +7,13 @@ export default class Player {
     this.xPos = xPos;
     this.yPos = yPos;
     this.p = p;
+    this.id = id;
   }
   show() {
+    console.log('SHOW');
     this.p.noStroke();
-    this.p.fill(0);
-    this.p.rect(this.x, this.y, this.h, this.w);
+    this.p.fill(200);
+    this.p.rect(this.x * this.xPos, (this.y * this.yPos) + this.y, this.h, this.w);
   }
 
-
-  //transfer to state
-  
-  // move(axis, dir) {
-
-  //   this[axis[0]] += dir;
-
-  }
-
-  //transfer to state
-  updateCoords(axis, dir, xyDir, moves) {
-
-
-    let newX = this.xPos;
-    let newY = this.yPos;
-
-    if(axis === 'xPos') {
-      newX += dir;
-    } else {
-      newY += dir;
-    }
-
-    let newCoords = [newX, newY];
-
-    if(moves.some(coords => {
-      return coords.x === newCoords[0] && coords.y === newCoords[1];
-    })
-    ) {
-      this.xPos = newX;
-      this.yPos = newY;
-
-      this.move(axis, xyDir);
-    }
-
-  }
 }
--- src/container/Game.js
+++ src/container/Game.js
@@ -42,7 +42,7 @@ const Game = ({ match, history }) => {
   return (
     <>
       <h1>GAME</h1>
-      <P5Wrapper sketch={sketch} state={eventState} />
+      <P5Wrapper sketch={sketch} players={eventState.room.players} cellMap={eventState.room.cellMap} />
     </>
   );
 };
